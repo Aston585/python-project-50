69cd0eb4b1f9cafa7d24619d65d524751a3c4309
Garkusha Valery <aston585v@gmail.com>
2023-03-15 21:47:45 +0300

    chore!: Problems with recursion

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,6 +1,9 @@
 import json
 import yaml
 
+file_path1 = './tests/fixtures/file1.json'
+file_path2 = './tests/fixtures/file2.json'
+
 
 def chek_and_open_file(file_path):
     if file_path.endswith('.yaml') or file_path.endswith('.yml'):
@@ -18,19 +21,23 @@ def chek_and_open_file(file_path):
 def generate_diff(file_path1, file_path2):
     source1 = chek_and_open_file(file_path1)
     source2 = chek_and_open_file(file_path2)
-    output = map(prepare_output_data,
-                 (i for i in get_comparison_results(source1, source2)))
-    result = '\n'.join(output)
-    return '{' + '\n' + result + '\n' + '}' + '\n'
+    # output = map(prepare_output_data,
+                 # (i for i in get_comparison_results(source1, source2)))
+    # result = '\n'.join(output)
+    # '{' + '\n' + result + '\n' + '}' + '\n'
+    return get_comparison_results( source1,  source2)
 
 
 def get_comparison_results(data1, data2):
     keys = data1.keys() | data2.keys()
     result = []
+    print(keys)
     for key in sorted(keys):
         if key not in data2:
             result.append(('-', key, data1[key]))
         elif key in data1 and data2:
+            if isinstance(data1[key], dict) and isinstance(data2[key], dict):
+                return get_comparison_results(data1[key], data2[key])
             if data1[key] == data2[key]:
                 result.append((' ', key,  data1[key]))
             elif data1[key] != data2[key]:
@@ -41,6 +48,9 @@ def get_comparison_results(data1, data2):
     return result
 
 
-def prepare_output_data(data):
-    status, key, value = data
-    return f"  {status} {key}: {value if value not in (False, True) else str(value).lower()}"  # noqa: E501
+# def prepare_output_data(data):
+    # status, key, value = data
+    # return f"  {status} {key}: {value if value not in (False, True) else str(value).lower()}"  # noqa: E501
+
+
+print(generate_diff(file_path1, file_path2))
