40867d5ec937222224ce86490d67cdf6331adce9
Garkusha Valery <aston585v@gmail.com>
2023-03-21 21:32:10 +0300

    feat: add correct diff and stylish

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,8 +1,6 @@
 import json
 import yaml
-
-file_path1 = './tests/fixtures/file1.json'
-file_path2 = './tests/fixtures/file2.json'
+from itertools import chain
 
 
 def chek_and_open_file(file_path):
@@ -21,36 +19,52 @@ def chek_and_open_file(file_path):
 def generate_diff(file_path1, file_path2):
     source1 = chek_and_open_file(file_path1)
     source2 = chek_and_open_file(file_path2)
-    # output = map(prepare_output_data,
-                 # (i for i in get_comparison_results(source1, source2)))
-    # result = '\n'.join(output)
-    # '{' + '\n' + result + '\n' + '}' + '\n'
-    return get_comparison_results( source1,  source2)
+    diff = get_comparison_results(source1, source2)
+    return stylish(diff)
 
 
 def get_comparison_results(data1, data2):
     keys = data1.keys() | data2.keys()
     result = []
-    print(keys)
     for key in sorted(keys):
         if key not in data2:
             result.append(('-', key, data1[key]))
         elif key in data1 and data2:
-            if isinstance(data1[key], dict) and isinstance(data2[key], dict):
-                return get_comparison_results(data1[key], data2[key])
-            if data1[key] == data2[key]:
-                result.append((' ', key,  data1[key]))
-            elif data1[key] != data2[key]:
-                result.append(('-', key,  data1[key]))
-                result.append(('+', key, data2[key]))
+            if not (isinstance(data1[key], dict)
+                    and isinstance(data2[key], dict)):
+                if data1[key] == data2[key]:
+                    result.append((' ', key,  data1[key]))
+                elif data1[key] != data2[key]:
+                    result.append(('-', key,  data1[key]))
+                    result.append(('+', key, data2[key]))
+            else:
+                result.append((' ', key,
+                               get_comparison_results(data1[key], data2[key])))
         else:
             result.append(('+', key, data2[key]))
     return result
 
 
-# def prepare_output_data(data):
-    # status, key, value = data
-    # return f"  {status} {key}: {value if value not in (False, True) else str(value).lower()}"  # noqa: E501
+def stylish(data, replacer=' ', spaces_count=4):
+
+    def inner(data, indent=0):
+        if not isinstance(data, list):
+            return data
+        level = replacer * indent
+        indent += spaces_count
+        a = list(map(lambda data: f"{level}{replacer * spaces_count}"
+                                  f"{data[0]} {data[1]}: "
+                                  f"{normalize_value(inner(data[2], indent))}", data))  # noqa: E501
+        b = list(chain('{', a, [level + '}']))
+        return '\n'.join(b)
+
+    return inner(data)
 
 
-print(generate_diff(file_path1, file_path2))
+def normalize_value(data):
+    if data is None:
+        return 'null'
+    if data not in (False, True):
+        return data
+    else:
+        return str(data).lower()
