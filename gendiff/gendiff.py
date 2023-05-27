7aa03f0a337ef59439f71746cd5796a2de3ed0f6
Garkusha Valery <aston585v@gmail.com>
2023-05-27 13:14:45 +0300

    ModuleNotFoundError: No module named 'gendiff.loading'

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,18 +1,34 @@
-from .loading import loading_data
-from .formaters.stylish import stylish_view
-from .formaters.plain import flatten
-from .formaters.json_ import json_viev
+from gendiff.loading import loading_data
+
+#from gendiff.formaters.stylish import stylish_view
+#from gendiff.formaters.plain import flatten
+#from gendiff.formaters.json_ import json_viev
+
+
+data1 = './tests/fixtures/file1.json'
+data2 = './tests/fixtures/file2.json'
 
 
 def generate_diff(data1, data2, format='stylish'):
     source1 = loading_data(data1)
     source2 = loading_data(data2)
-    diff = get_comparison_results(source1, source2)
-    if format == 'plain':
-        return flatten(diff)(diff)
-    elif format == 'json':
-        return json_viev(diff)
-    return stylish_view(diff)
+    #diff = get_comparison_results(source1, source2)
+    #if format == 'plain':
+        #return flatten(diff)(diff)
+    #elif format == 'json':
+        #return json_viev(diff)
+    pass
+    #return stylish_view(diff)
+
+
+print(generate_diff(data1, data2))
+
+
+
+
+
+
+
 
 
 def get_comparison_results(data1, data2):
@@ -21,16 +37,16 @@ def get_comparison_results(data1, data2):
     for key in sorted(keys):
         if (key in data1) and (key in data2):
             if isinstance(data1[key], dict) and isinstance(data2[key], dict):
-                result.append({'status': 'unchanging',
+                result.append({'status': 'nested',
                                'key': key,
                                'value': normalize_value(get_comparison_results(
                                         data1[key], data2[key]))})
             elif data1[key] == data2[key]:
-                result.append({'status': 'unchanging',
+                result.append({'status': 'unchanged',
                                'key': key,
                                'value': normalize_value(data1[key])})
             elif data1[key] != data2[key]:
-                result.append({'status': 'changing',
+                result.append({'status': 'changed',
                                'key': key,
                                'from': normalize_value(data1[key]),
                                'to': normalize_value(data2[key])})
